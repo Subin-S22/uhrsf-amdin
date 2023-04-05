d6cb9c6e6f1de6def0c6ce374dc093502c4ff1da
subin.s22@outlook.com <Subin-S22>
2023-04-05 21:01:18 +0530

    url change

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAxios = axios.create({
-  baseURL: "http://csnservernet.tech/uhrsf_dev/api/v1/uhrsf/",
+  baseURL: "https://csnservernet.tech/uhrsf_dev/api/v1/uhrsf/",
 });
 
 // Add a request interceptor
